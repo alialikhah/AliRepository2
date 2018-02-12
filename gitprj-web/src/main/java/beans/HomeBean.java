3594a88bb1593e789256bef54492d06a537e7f0d
fedoraali <fedoraali@FedoraAli>
2018-02-12 12:56:38 +0330

    first

---
+++ gitprj-web/src/main/java/beans/HomeBean.java
@@ -0,0 +1,22 @@
+package beans;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class HomeBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public HomeBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
